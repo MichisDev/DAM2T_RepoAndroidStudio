3e125fc4aa9495b1ae2fdbb2ce08fa47c3440806
MichisDev <150479444+MichisDev@users.noreply.github.com>
2024-10-03 17:29:56 +0200

    seee finiiii

--- PMDM_UT01_Calculadora/app/src/main/java/com/example/pmdm_ut01_calculadora/MainActivity.kt
+++ PMDM_UT01_Calculadora/app/src/main/java/com/example/pmdm_ut01_calculadora/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.pmdm_ut01_calculadora
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +18,26 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        // Botones
+        val btSumar = findViewById<Button>(R.id.btSumar)
+        val btRestar = findViewById<Button>(R.id.btRestar)
+        val btMultiplicar = findViewById<Button>(R.id.btMultiplicar)
+        val btDividir = findViewById<Button>(R.id.btDividir)
+
+
+        // EditText
+        val etnNumero1 = findViewById<EditText>(R.id.etnNumero1)
+        val etnNumero2 = findViewById<EditText>(R.id.etnNumero2)
+        val etnResultado = findViewById<EditText>(R.id.etnResultado)
+
+        // Eventos de los botones
+        btSumar.setOnClickListener {
+           val num1 = etnNumero1.text.toString().toInt()
+        }
+        btRestar.setOnClickListener {   }
+        btMultiplicar.setOnClickListener {   }
+        btDividir.setOnClickListener {   }
+
     }
 }
