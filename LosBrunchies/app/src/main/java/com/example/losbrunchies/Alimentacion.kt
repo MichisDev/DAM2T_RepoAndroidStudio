06221f1eff49c9f482392100c67e2b2d253a091b
MichisDev <150479444+MichisDev@users.noreply.github.com>
2024-11-30 18:36:58 +0100

    forrmulario

--- LosBrunchies/app/src/main/java/com/example/losbrunchies/Alimentacion.kt
+++ LosBrunchies/app/src/main/java/com/example/losbrunchies/Alimentacion.kt
@@ -4,6 +4,9 @@ import adaptador.RecetaAdapter
 import android.os.Bundle
 import android.view.WindowManager
 import android.widget.AutoCompleteTextView
+import android.widget.SeekBar
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
@@ -17,6 +20,7 @@ import androidx.recyclerview.widget.PagerSnapHelper
 import androidx.recyclerview.widget.RecyclerView
 import com.example.losbrunchies.databinding.ActivityAlimentacionBinding
 import com.example.losbrunchies.databinding.CrearRecetaBinding
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import modelo.Receta
 import modelo.RecetaData
 
@@ -24,8 +28,10 @@ class Alimentacion : AppCompatActivity() {
 
     private lateinit var binding: ActivityAlimentacionBinding
     private lateinit var recetas: MutableList<Receta>
+
     // Agregar binding para el diálogo
     private lateinit var dialogBinding: CrearRecetaBinding
+
     // combobox
     private lateinit var tvCombobox: AutoCompleteTextView
 
@@ -40,7 +46,8 @@ class Alimentacion : AppCompatActivity() {
         recetas = RecetaData.obtenerRecetas().toMutableList()
 
         var recyclerView: RecyclerView = binding.rvRecetas
-        recyclerView.layoutManager = CustomLinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+        recyclerView.layoutManager =
+            CustomLinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
         val adapter = RecetaAdapter(recetas)
         recyclerView.adapter = adapter
 
@@ -58,23 +65,51 @@ class Alimentacion : AppCompatActivity() {
     }
 
     private fun mostrarCrearRecetaDialog() {
-        val builder = AlertDialog.Builder(this)
-        dialogBinding = CrearRecetaBinding.inflate(layoutInflater) // Inflar layout del diálogo con binding
-        builder.setView(dialogBinding.root)
 
-        val dialog = builder.create()
-        dialog.show()
+        dialogBinding = CrearRecetaBinding.inflate(layoutInflater)
 
-        dialog.window?.setLayout(
+        val builder = MaterialAlertDialogBuilder(this)
+            .setTitle("Crea tu propia receta")
+            .setMessage("Mensaje de receta")
+            .setView(dialogBinding.root)
+            .setNegativeButton("Cancelar") { dialog, which ->
+                Toast.makeText(this, "Creación de receta cancelada", Toast.LENGTH_SHORT).show()
+                dialog.dismiss()
+            }
+            .setPositiveButton("Aceptar") { dialog, which ->
+                Toast.makeText(
+                    this,
+                    "Esto añadirá la receta a la lista de recetas. Función en construcción",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+
+        val dialog = builder.create() // Crear el diálogo
+        dialog.show() // Mostrar el diálogo
+
+        val window = dialog.window
+        window?.setLayout(
             WindowManager.LayoutParams.MATCH_PARENT,
             WindowManager.LayoutParams.MATCH_PARENT
         )
 
-        // Acceder a los elementos y manejar interacciones usando dialogBinding
-        // dialogBinding.tvNombreRec.text
-        // ...
+        val seekBar = dialogBinding.sbTiempo
+        val textView = dialogBinding.tvValorSeekBar
+
+        seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                textView.text = progress.toString()
+            }
 
 
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
 
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+
+            }
+
+        })
     }
 }
